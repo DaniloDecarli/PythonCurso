a4f01bdba90a10964bd545f76946a54db2e9b46d
Danilo Decarli <danilosdecar@gmail.com>
2021-12-27 14:49:15 -0300

    Adicao de novos arquivos

--- Aula 7/exercício14.py
+++ Aula 7/exercício14.py
@@ -5,6 +5,6 @@ fahrenheit = (celsius * 1.8) + 32
 kelvin = celsius + 273.15
 print('{}°C corresponde há: {}°F e {}K !'.format(celsius, fahrenheit, kelvin))
 
-
+# Simplificado 
 cel = float(input('Digite a temperatura em °C: '))
 print('{}°C corresponde há: {}°F e {}K !'.format(cel, (celsius * 1.8) + 32, cel + 273.15))
---
+++ Aula 9/Aula.py
@@ -0,0 +1,7 @@
+frase = 'Curso em Vídeo Python'
+print(len(frase))
+print(frase[0:22])
+print("""Hoje sei um pouco mais de programação, estou feliz com meu desenvolvoimento.
+E respeitando meus limites.""")
+print('Vídeo' in frase)
+print('Escola' in frase)
