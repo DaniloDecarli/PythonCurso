a6e3b68e302f7e8dcd64893e26e9988fa75813f9
Danilo Decarli <danilosdecar@gmail.com>
2021-12-27 17:47:11 -0300

    adicao de arquivos

---
+++ Aula 9/Exercício 23.py
@@ -0,0 +1,16 @@
+# Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+
+num = int(input('Informe um numero: '))
+print('Analisando o número: {}'.format(num))
+print('Seu numero tem: ')
+print('Unidade: {}'.format(num // 1 % 10))
+print('Dezena: {}'.format(num // 10 % 10))
+print('Centena: {}'.format(num // 100 % 10))
+print('Milhar: {}'.format(num // 1000 % 10))
+#n = str(num)
+#print('Analisando o número: {}'.format(num))
+#print('Seu numero tem: ')
+#print('Unidade: {}'.format(n[3]))
+#print('Dezena: {}'.format(n[2]))
+#print('Centena: {}'.format(n[1]))
+#print('Milhar: {}'.format(n[0]))
