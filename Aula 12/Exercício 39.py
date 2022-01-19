7b0f0c9fb48f0f04ddb46f4a75f15e95dd569a12
Danilo Decarli <danilosdecar@gmail.com>
2022-01-19 16:50:49 -0300

    adicao de novos arquivos

--- Aula 12/Exercício 39.py
+++ Aula 12/Exercício 39.py
@@ -4,13 +4,15 @@ from time import sleep
 from datetime import date
 print('*' * 20, 'Programa de Alistamento', '*' * 20)
 nome = str(input('Informe seu primeiro nome: '))
-ano = int(input('Informe o ano de nascimento: '))
+idade = int(input('Informe o ano de nascimento: '))
 sleep(1)
-idade = date.today().year - ano
-if idade < 18:
-    print('Faltam {} anos para seu alistamento.'.format(18 - idade))
-elif idade > 18:
-    print('Você já colaborou com nosso País Parabéns!!! está com {} anos'.format(idade)) 
+liste = date.today().year - idade
+if liste < 18:
+    print('Faltam {} anos para seu alistamento {}.'.format((18 - liste), nome))
+    print('Você se alistará em {}'.format((18 - liste) + (date.today().year)))
+elif liste > 18:
+    print('Você já colaborou com nosso País {} Parabéns!!! está com {} anos'.format(nome, liste)) 
+    print('Você serviu ao País em {}'.format((date.today().year) - (liste - 18))) 
 else:
-    print('Parabéns você está com {} anos, seu ano de alistamento! seja bem vindo recruta!'.format(idade))
-print('-=-' * 60)           
+    print('Parabéns {} você está com {} anos, esse é seu ano de alistamento! seja bem vindo recruta!'.format(nome, liste))
+print('-=-' * 60)     
