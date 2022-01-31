c840f2ec2111ca5a0a0a441a5467122e82d88e70
Danilo Decarli <danilosdecar@gmail.com>
2022-01-31 15:09:29 -0300

    adicao de arquivos

---
+++ Aula 13/Exercício 50.py
@@ -0,0 +1,14 @@
+# Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+
+
+
+from time import sleep
+print('Digite sei numeros inteiros.')
+sleep(0.5)
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input('Digite º{} numero: '.format(c)))
+    soma = soma + num
+    cont = cont + 1
+print('Você informou {} números e a soma foi {}'.format(cont, soma))    
