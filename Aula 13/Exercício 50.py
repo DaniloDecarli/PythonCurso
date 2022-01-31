eca77e29e30d9aa53e10b397dedcd24016d38b1a
Danilo Decarli <danilosdecar@gmail.com>
2022-01-31 15:12:29 -0300

    adicao de arquivos

--- Aula 13/Exercício 50.py
+++ Aula 13/Exercício 50.py
@@ -9,6 +9,7 @@ soma = 0
 cont = 0
 for c in range(1, 7):
     num = int(input('Digite º{} numero: '.format(c)))
-    soma = soma + num
-    cont = cont + 1
-print('Você informou {} números e a soma foi {}'.format(cont, soma))    
+    if num % 2 == 0:
+       soma = soma + num
+       cont = cont + 1
+print('Você informou {} números PARES, e a soma foi {}'.format(cont, soma))    
